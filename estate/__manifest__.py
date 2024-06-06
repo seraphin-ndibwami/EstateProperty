6bdd9aba003d744245d3372bb6f6e2b4c800c7cc
seraphin-ndibwami <sndibwami@virunga.org>
2024-06-06 16:06:10 +0200

    [ADD] state: Add a new real estate management module
    - Create base structure (__init__.py, __manifest__.py)
    
    We have noticed the absence of a module in Odoo that allows for the proper management of real estate propertiesThis commit introduces a new module for real estate management, including the creation, update, and deletion of properties and associated offers.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "estate",
+    'version': "1.0",
+    'license': "AGPL-3",
+    'depends': ['base'],
+    'author': "sndibwami@virunga.org",
+    'summary': "Real Estate Management",
+    'application': True,
+    'installable': True,
+}
