c54e497eb78de9a8e88a675f508cce813522bbc7
seraphin-ndibwami <sndibwami@virunga.org>
2024-06-07 10:33:24 +0200

    [IMP] estate: configure basic list, form and search views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,13 +5,16 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
+    def _in_three_months(self):
+        return fields.Date.add(fields.Date.context_today(self), months=3)
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=_in_three_months)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -23,3 +26,11 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], default='new')
